29fa99df93cf553993751feee8a1252158551a50
Pritham13 <prithamhsuvarna@gmail.com>
2024-10-06 12:07:14 +0530

    updated stack initialization

--- kernel/inc/tash.h
+++ kernel/inc/tash.h
@@ -1,3 +1,22 @@
 #include <stdint.h>
-#define STACK
-void create_task (void ( * function_ptr )( void ) , char *taskName , uint8_t priority , uint16_t stackSize )  
+#define THREAD_STACK_SIZE  2048
+#define MY_EXC_RETURN 0xFFFFFFFD  /* Return to thread mode using process stack and restore basic stack frame */
+#define NORM_STACK_FRAME_SIZE           8
+#define NORM_STACK_FRAME_XPSR_OFFSET    7
+#define NORM_STACK_FRAME_PC_OFFSET      6
+#define NORM_STACK_FRAME_LR_OFFSET      5
+#define NORM_STACK_FRAME_R12_OFFSET     4
+#define NORM_STACK_FRAME_R3_OFFSET      3
+#define NORM_STACK_FRAME_R2_OFFSET      2
+#define NORM_STACK_FRAME_R1_OFFSET      1
+#define NORM_STACK_FRAME_R0_OFFSET      0
+#define DEF_MY_XPSR                     0x01000000
+#define MAX_MPU_REGIONS                 8
+
+uint8_t threadID = 0 ;
+
+enum task_states {
+    READY,
+    SUSPENDED,
+}
+void create_task (void ( * function_ptr )( void ) , char *taskName , uint8_t priority , uint16_t stackSize ) ;
--- kernel/src/task.c
+++ kernel/src/task.c
@@ -4,12 +4,18 @@
 
 
 void create_task (void ( * function_ptr )( void ) , char *taskName , uint8_t priority , uint16_t stackSize ) { 
+  tcb *newTcb ;
   newTcb = (tcb *) malloc(sizeof(tcb)) ;
   newTcb->taskName = ( char * ) malloc ( 20 );
   memset ( newTcb->taskName, '\0', 20 );
   strcpy ( newTcb->taskName, taskname );
   newTcb -> priority = priority ;
-  newTcb -> stackPtr =   stackSize + THREAD_STACK_SIZE - NORM_STACK_FRAME_SIZE;
+  // newTcb -> stackPtr =   stackSize + THREAD_STACK_SIZE - NORM_STACK_FRAME_SIZE;
+  newTcb -> stackPtr = (uint32_t *) malloc(stackSize * sizeof(int32_t)) ;
+  newTcb -> funcPtr = function_ptr;
+  newTcb -> threadID = threadID + 1 ;
   
+  for (int i = 0 ; i < NUM_OF_CALEE_REGS ; i++ ) 
+    newTcb -> calleeRegs[i] = 0 ;
   
 }

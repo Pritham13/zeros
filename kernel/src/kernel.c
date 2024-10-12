b4c683be2ee0330a75a36a6f8da5c1d8967554f6
zero <prithamhsuvarna@gmail.com>
2024-10-13 01:56:39 +0530

    adding OS init and Thread launcher functions

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -1,43 +1,64 @@
 #include "../inc/kernel.h"
 #include "../../main/sysCalls.h"
-tcb *currentTcb ;
 
 uint32_t Context_Switch(uint32_t lr , uint32_t *caleeRegs ) {
   
-  for (int i = 0 ; i < NUM_OF_CALEE_REGS; i++ ) {
-    currentTcb -> calleeRegs[i] = caleeRegs[i];
-  }
-  currentTcb -> lr = lr ;
-  currentTcb->stackPtr = __get_PSP() ; // gets the Processor stack Pointer for the current task
-  /* Scheduling block to be added later */
-  if(currentTcb == tcbThreadA)
+    for (int i = 0 ; i < NUM_OF_CALEE_REGS; i++ ) {
+        currentTcb -> calleeRegs[i] = caleeRegs[i];
+    }
+    currentTcb -> lr = lr ;
+    currentTcb->stackPtr = __get_PSP() ; // gets the Processor stack Pointer for the current task
+    /* Scheduling block to be added later */
+    if(currentTcb == tcbThreadA)
     currentTcb = *tcbThreadB ;
-  
-  else if(currentTcb == tcbThreadB)
+
+    else if(currentTcb == tcbThreadB)
     currentTcb = *tcbThreadA ;
-  
-  else
+
+    else
     return -1 ; // indicates error in the thread will be changed to somethng else
 
-  /* end of Scheduling block */
+    /* end of Scheduling block */
 
-   for (int i = 0 ; i < NUM_OF_CALEE_REGS; i++ ) {
+    for (int i = 0 ; i < NUM_OF_CALEE_REGS; i++ ) {
     caleeRegs[i] = currentTcb -> calleeRegs[i] ;
-  }
-  /* Set PSP and PSP Limit */
-  __set_PSPLIM ((uint32_t)(currentTcb -> stackLimit));
-  __set_PSP((uint32_t)(currentTcb -> stackPtr));
-  return currentTcb -> lr ;
+    }
+    /* Set PSP and PSP Limit */
+    __set_PSPLIM ((uint32_t)(currentTcb -> stackLimit));
+    __set_PSP((uint32_t)(currentTcb -> stackPtr));
+    return currentTcb -> lr ;
 
 }
 
 void OS_init (void) { 
-  systick_init( ) ; // Systick initialization put the clock value later
-  
+    systick_init( ) ; // Systick initialization put the clock value later
+
 }
 
+uint32_t thread_launcher (uint32_t lr , uint32_t *calleeRegs) {
+	
+	for (int i = 0 ; i < NUM_OF_CALEE_REGS; i++ ) {
+        calleeRegs[i] = currentTcb ->calleeRegs[i];
+    }
+    __set_PSPLIM ((uint32_t)(currentTcb -> stackLimit));
+    __set_PSP((uint32_t)(currentTcb -> stackPtr));
+    return currentTcb -> lr ;
+}
+
+__attribute__ ((naked)) void Start_OS (void) {
+   __asm(
+        "CPSID I\n"
+        "PUSH    {r4-r11}\n"
+        "MOV     r1,sp\n"
+        "MOV     r0,lr\n"
+        "BL      thread_launcher\n"
+        "POP     {r4-r11}\n"
+        "BX      r0\n" //NOTE: This line has to be checked for functionatlity
+        "CPSIE I"
+    ); 
+}
 extern __attribute__((naked)) void SysTick_Handler(void) {
-  SWITCH_STATE;
+    SWITCH_STATE;
 }
 
 void OS_start (void) {
